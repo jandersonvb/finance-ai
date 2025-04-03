7c108df8306ebd8940d547bc9e9ba557d827d9d0
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-04-02 22:11:37 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    </div>
+  )
 }
 
 export default Home
