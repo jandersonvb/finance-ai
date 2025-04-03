03f4a14d0bf49d3f5ee8e41d8a2b7581c0bd06d2
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-04-02 22:49:20 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
   return (
-    <div className="flex w-screen items-center justify-center">
+    <div className="flex w-screen items-center justify-center flex-col gap-4 p-24">
       <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+
+      <Button>Botão</Button>
     </div>
   )
 }
