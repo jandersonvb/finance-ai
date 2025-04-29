1cda377fa4a4c336b313029b8f61973f0c2e9cb6
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-04-29 00:03:08 -0300

    chore: add shadcn/ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <h1 className="px-5 text-red-500">Hello World</h1>
+
+      <Button>Botão do Shadcn</Button>
     </div>
   );
 }
