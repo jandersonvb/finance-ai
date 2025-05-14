52ac6b866e466a430984f0c05f14fff2c51902c0
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-05-13 23:13:14 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid grid-cols-2 h-full">
       <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
@@ -15,10 +24,13 @@ export default function LoginPage() {
         />
         <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
         <p className="text-muted-foreground mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login
+          </Button>
+        </SignInButton>
+
       </div>
 
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <h1 className="px-5 text-red-500">Hello World</h1>
 
-      <Button>Botão do Shadcn</Button>
+      <UserButton showName />
     </div>
   );
 }
